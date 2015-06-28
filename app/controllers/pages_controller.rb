7064b9731efb8e08e7858019425b2656e59842e2
Caitlyn Yu <caitlynmyu@gmail.com>
2015-06-27 19:39:23 -0700

    add user-specific timeline, need to change view to show login page if not logged in

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,13 @@
 class PagesController < ApplicationController
   def index
+    client = Twitter::REST::Client.new do |config|
+      config.consumer_key        = "WfLCBqb41OjVEGy42s5V54ivt"
+      config.consumer_secret     = "3dcJA2ZV5Q245C8eMwHji6Sx5aw2TkEKYS4DovCsgfwMm4CZLy"
+      config.access_token        = "417629465-mQNDWCwQacruc5WUKfs0yupfBdQbN14wekAP7JTV"
+      config.access_token_secret = "M61vICNvYccgrR4zvn6BlqUnbbM0HmpNo5iD7STFF0xvH"
+    end
+
+    @timeline = client.home_timeline
   end
 
   def show
