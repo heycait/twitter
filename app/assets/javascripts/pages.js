886cc6a3e8d8406add1dd92864aca8309dc05b3a
Caitlyn Yu <caitlynmyu@gmail.com>
2015-06-29 15:14:45 -0700

    change get route to post

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
   $('#tweet-form').on('submit', function(e){
     e.preventDefault();
     request = $.ajax({
+      method: "post",
       url: $(this).attr("action"),
       data: {tweet: $('input[name="tweet"]').val()},
     });
